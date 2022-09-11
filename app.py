b5a31d1ba2c510159cfb0aa3f7dc498bac4fef1c
RJuro <roman.j@me.com>
2022-09-11 22:17:03 +0200

    -

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
 import streamlit as st
+import streamlit.components.v1 as components
+import pydeck as pdk
 
-st.write(""" My first app!!! Yeah!
-""")
+import numpy as np
+import pandas as pd 
 
-st.markdown('# This is awesome 🚀')
+#import seaborn as sns #seaborn til plots
+from matplotlib import pyplot as plt #plot control
+
+
+from jupyterthemes import jtplot
+jtplot.style(theme='monokai', context='notebook', ticks=True, grid=False)
+
+
+st.set_page_config(page_title='Streamlit - Dashboard 🤯',
+page_icon="🚀",
+layout='wide'
+)
